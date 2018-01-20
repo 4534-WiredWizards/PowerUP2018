8873a0b504491fca7eb718dce105748f39509805
NormallyNormal <33298484+NormallyNormal@users.noreply.github.com>
2018-01-20 14:15:10 -0500

    Made reset gyro command, fixed timer problem in box ejection.

--- src/Commands/ResetGyro.cpp
+++ src/Commands/ResetGyro.cpp
@@ -34,12 +34,12 @@ void ResetGyro::Execute() {
 
 // Make this return true when this Command no longer needs to run execute()
 bool ResetGyro::IsFinished() {
-    return false;
+    return true;
 }
 
 // Called once after isFinished returns true
 void ResetGyro::End() {
-
+	Robot::driveTrain->resetGyroAngle();
 }
 
 // Called when another command which requires one or more of the same

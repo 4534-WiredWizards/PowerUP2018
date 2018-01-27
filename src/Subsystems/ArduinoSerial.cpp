786ec00f3507964be9af7f9dee13d2ae041af0ef
Paige <dooda29@gmail.com>
2018-01-27 17:29:11 -0500

    Created ArduinoSerial subsystem ;)

---
+++ src/Subsystems/ArduinoSerial.cpp
@@ -0,0 +1,27 @@
+#define MAX_INPUT_CHAR 32
+
+#include "ArduinoSerial.h"
+#include "../RobotMap.h"
+
+//#include "SerialPort.h"
+
+ArduinoSerial::ArduinoSerial() : Subsystem("ArduinoSerial") {
+	serialPort.reset(new SerialPort(115200, SerialPort::kMXP));
+	inputBuffer=(char*)calloc(MAX_INPUT_CHAR, sizeof(char));
+}
+
+void ArduinoSerial::InitDefaultCommand() {
+	// Set the default command for a subsystem here.
+	// SetDefaultCommand(new MySpecialCommand());
+}
+std::string ArduinoSerial::Read(){
+	serialPort->Read(inputBuffer, MAX_INPUT_CHAR);
+	return inputBuffer;
+}
+void ArduinoSerial::Write(std::string input){
+	serialPort->Write(input);
+}
+
+
+// Put methods for controlling this subsystem
+// here. Call these from Commands.
---
+++ src/Subsystems/ArduinoSerial.h
@@ -0,0 +1,21 @@
+#ifndef ArduinoSerial_H
+#define ArduinoSerial_H
+
+#include <Commands/Subsystem.h>
+#include "WPILib.h"
+
+class ArduinoSerial : public Subsystem {
+private:
+	// It's desirable that everything possible under private except
+	// for methods that implement subsystem capabilities
+	std::shared_ptr<frc::SerialPort> serialPort;
+	char* inputBuffer;
+public:
+	ArduinoSerial();
+	void InitDefaultCommand();
+	std::string Read();
+	void Write(std::string input);
+
+};
+
+#endif  // ArduinoSerial_H

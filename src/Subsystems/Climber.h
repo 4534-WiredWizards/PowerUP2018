37d22f688b414569d209baf02ee95404c47bd244
maela <maela@LAPTOP-KMEPR59U.ec.rr.com>
2018-02-08 20:01:59 -0500

    Start of a grand climber subsystem

---
+++ src/Subsystems/Climber.h
@@ -0,0 +1,18 @@
+#ifndef Climber_H
+#define Climber_H
+
+#include <Commands/Subsystem.h>
+#include "WPILib.h"
+class Climber : public Subsystem {
+private:
+	std::shared_ptr<frc::SpeedController> climberMotor;
+	// It's desirable that everything possible under private except
+	// for methods that implement subsystem capabilities
+
+public:
+	Climber();
+	void SetMotor();
+	void InitDefaultCommand();
+};
+
+#endif  // Climber_H
